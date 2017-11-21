7c3de70fb4c2d9515612064e031a7ecc85c2d521
Kozacki <lukasz.kozak96@hotmail.com>
2017-11-21 19:42:01 +0100

    0.1.2
    System  logowania z uwzględniając sesje

--- gra.php
+++ gra.php
@@ -17,6 +17,12 @@ session_start();
 
 <?php
 
-echo "<p>Witaj".$_SESSION['user']."!</p>";
+echo "<p>Witaj ".$_SESSION['user']."!</p>";
+echo "<p><b> Drewno</b>: ".$_SESSION['drewno'];
+echo "|<b> Kamień</b>: ".$_SESSION['kamien'];
+echo "|<b> Zboże</b>: ".$_SESSION['zboze'];
+echo "</br></br>";
+echo "<b>e-mail</b>: ".$_SESSION['email'];
+echo "</br><b>Dni premium</b>: ".$_SESSION['dnipremium']."</p>";
 
 ?>
--- zaloguj.php
+++ zaloguj.php
@@ -1,3 +1,4 @@
+{
 <?php
 session_start();
 require_once "connect.php";
@@ -9,7 +10,6 @@ if($connecting->connect_errno!=0)
 echo "Error: ".$connecting->connect_errno;
 }
 else
-{
 $login = $_POST['login'];
 $password = $_POST['password'];
 
@@ -19,29 +19,30 @@ $sql = "SELECT * FROM uzytkownicy WHERE user='$login' AND pass='$password'";
 if($result = @$connecting->query($sql))
 {
 	$ile_userow = $result->num_rows;
-	if($ile_userow = 1)
+	if($ile_userow > 0)
 	{
-		$row = $result->fetch_assoc();
-		$_SESSION['user'] = $row['user'];
+		$_SESSION['zalogowany'] = true;
 
-		
+		$row = $result->fetch_assoc();
+		$_SESSION['id'] = $row['id'];
+		$_SESSION['user'] = $row['user'];
+		$_SESSION['drewno'] = $row['drewno'];
+		$_SESSION['kamien'] = $row['kamien'];
+		$_SESSION['zboze'] = $row['zboze'];
+		$_SESSION['email'] = $row['email'];
+		$_SESSION['dnipremium'] = $row['dnipremium'];
+
+		unset($_SESSION['blad']);
 		$result->close();
 		header('Location:gra.php');
 	}
 	else
 	{
-	
+		$_SESSION['blad'] = '<span style="color:red">Nieprawidłowy login lub hasło!</span>';
+		header('Location:index.php');
 	}
 }
 
 $connecting->close();
-
-}
-
-
-
-
-
-
 
 ?>
